7caebeeef4b9246f543521dfcf8b7a1f861ed254
Jose Carlos Mena <jcme0003@red.ujaen.es>
2021-08-20 17:04:27 +0200

    First commit

---
+++ sw.js
@@ -0,0 +1,42 @@
+var myCache = 'testApp-v1';
+var files = [
+    './',
+    './index.html',
+    './manifest.webmanifest',
+    './src/phaser.min.js',
+    './def/phaser.d.ts',
+    './src/load-sw.js',
+    './src/main.js',
+    './src/installable.js',
+    './assets/icon-192.png',
+    './assets/icon-256.png',
+    './assets/icon-512.png'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(myCache).then(function(cache) {
+            return cache.addAll(files);
+        })
+    )
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if(key !== myCache) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
